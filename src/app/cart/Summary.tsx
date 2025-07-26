434f74b478d67c36b3f06c5be4f56bf9f4edd83c
Faosy Adebimpe <faosyadebimpe@gmail.com>
2025-07-26 07:42:26 -0700

    add checkout summary

---
+++ src/app/cart/Summary.tsx
@@ -0,0 +1,119 @@
+'use client';
+
+import classNames from 'classnames';
+import { ChevronDownIcon } from 'lucide-react';
+import Link from 'next/link';
+import { useState } from 'react';
+
+const Summary = ({ totalPrice }: { totalPrice: number }) => {
+    const [showPromoEntry, setShowPromoEntery] = useState(false);
+
+    return (
+        // <div className="hidden lg:flex flex-col items-start min-w-1/3">
+        <div className="hidden lg:flex flex-col items-start w-1/3">
+            <h2 className="font-semibold text-xl py-3">Summary</h2>
+
+            <div className="w-full m-0 p-0 mt-6">
+                <div className="flex gap-3 justify-between">
+                    <h5 className="font-semibold">Do you have a Promo Code?</h5>
+                    <button
+                        className=""
+                        onClick={() => setShowPromoEntery((prev) => !prev)}
+                    >
+                        <ChevronDownIcon
+                            className={classNames(
+                                'size-6 transition-transform',
+                                { '-rotate-180': showPromoEntry }
+                            )}
+                        />
+                    </button>
+                </div>
+
+                <div
+                    className={classNames(
+                        'w-full flex items-center gap-3 text-gray-500 transition-all overflow-hidden',
+                        {
+                            ' mb-7 mt-2 max-h-screen': showPromoEntry,
+                            'max-h-0': !showPromoEntry,
+                        }
+                    )}
+                >
+                    <input
+                        type="text"
+                        className="px-3 py-2 rounded-lg border border-gray-300 outline-none flex-auto"
+                    />
+                    <button className="px-5 py-2 rounded-full border border-gray-300 font-semibold">
+                        Apply
+                    </button>
+                </div>
+            </div>
+
+            <ul className="w-full flex gap-1 flex-col items-start mt-1 font-semibold">
+                <li className="w-full flex gap-1 justify-between items-center">
+                    <p>Subtotal</p>
+                    <p>${totalPrice}</p>
+                </li>
+                <li className="w-full flex gap-1 justify-between items-center">
+                    <p>Estimated Shipping & Handling</p>
+                    <p>Free</p>
+                </li>
+                <li className="w-full flex gap-1 justify-between items-center">
+                    <p>Estimated Tax</p>
+                    <p>-</p>
+                </li>
+                <li className="w-full flex gap-1 justify-between items-center">
+                    <p>Discount Total</p>
+                    <p>-</p>
+                </li>
+                <li className="w-full flex gap-1 justify-between items-center">
+                    <p className="text-green-500">0% off</p>
+                    {/* <p>-</p> */}
+                </li>
+
+                {/* total */}
+                <hr className="hr my-3" />
+                <li className="w-full flex gap-1 justify-between items-center">
+                    {/* <p className="text-green-500">Total</p> */}
+                    <p className="">Total</p>
+                    <p>${totalPrice}</p>
+                </li>
+                <hr className="hr my-3" />
+
+                <li className="font-normal">
+                    You qualify for{' '}
+                    <span className="font-semibold">Free Shipping</span> as a
+                    member!{' '}
+                    <Link
+                        href="#"
+                        className="underline underline-offset-2 font-semibold text-gray-500"
+                    >
+                        Join us
+                    </Link>{' '}
+                    or{' '}
+                    <Link
+                        href="#"
+                        className="underline underline-offset-2 font-semibold text-gray-500"
+                    >
+                        Sign-in
+                    </Link>
+                </li>
+
+                <li className="w-full flex gap-2 items-center">
+                    <div className="w-full h-2 flex-auto bg-green-800 rounded-full"></div>
+                    <p className="">$50</p>
+                </li>
+
+                <li className="w-full mt-5 flex">
+                    <Link
+                        href="#"
+                        className="w-full py-5 px-1 font-bold text-white bg-black rounded-full btn cc"
+                    >
+                        Checkout
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    );
+};
+
+export default Summary;
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,28 +2,35 @@
 
 import { useCartStore } from '@/store/cartStore';
 import Cart from './Cart';
+import Summary from './Summary';
 
 const CartPage = () => {
     const { getTotal, getTotalPrice } = useCartStore();
     const totalItems = getTotal();
     const totalPrice = getTotalPrice();
     return (
-        <div>
-            <div className="text-center mb-5 p-3">
-                <h2 className="text-2xl">Bag</h2>
-                <p className="text-gray-600">
-                    {totalItems} Items | ${totalPrice}
-                </p>
+        <div className="flex gap-7 p-3 max-w-[1094px] mx-auto">
+            <div className="flex-auto">
+                <div className="text-center mb-5 p-3 lg:px-0">
+                    <h2 className="text-2xl font-semibold text-gray-800 lg:text-left">
+                        Bag
+                    </h2>
+                    <p className="text-gray-600 lg:hidden font-semibold">
+                        <span className="">{totalItems}</span> Items | ${' '}
+                        <span className="text-gray-700">{totalPrice}</span>
+                    </p>
+                </div>
+
+                <hr className="w-full text-gray-300 mb-10 lg:hidden" />
+
+                {/* empty cart */}
+                {totalItems <= 0 && (
+                    <p className="mt-2">There are not items in your bag.</p>
+                )}
+
+                <Cart />
             </div>
-
-            <hr className="w-full text-gray-300 mb-10" />
-
-            {/* empty cart */}
-            {totalItems <= 0 && (
-                <p className="mt-2">There are not items in your bag.</p>
-            )}
-
-            <Cart />
+            <Summary totalPrice={totalPrice} />
         </div>
     );
 };
